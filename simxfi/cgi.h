a24bad1421392d6ecf191dcd573acc86ed180fe9
pmekomo <pierremartinpatrick.ekomo@gmail.com>
2016-07-18 16:41:25 +0200

    Ajout du programme formattant la page web pour le CGI

---
+++ simxfi/cgi.h
@@ -0,0 +1,9 @@
+#ifndef CGI_H
+#define CGI_H
+#include <iostream>
+#include <string>
+using namespace std;
+void head(string);
+void foot();
+
+#endif
---
+++ simxfi/main.cpp
@@ -0,0 +1,15 @@
+#include "cgi.h"
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	cout<<"Content-Type: text/html;\n\n";
+	head("Ma page en C !");
+	
+	cout<<"Hello World !";
+	
+	foot();
+	return 0;
+}
