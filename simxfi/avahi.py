f856504292c19457d91499bead30c09e9b55a732
Pierre-Martin EKOMO <pierremartinpatrick.ekomo@gmail.com>
2017-06-09 15:28:56 +0200

    Update avahi.py
    Ajout de commentaires explicatifs

--- simxfi/avahi.py
+++ simxfi/avahi.py
@@ -1,10 +1,16 @@
 #! /usr/bin/python
 
+#Ce script est utilisé afin de générer un fichier tmp.txt 
+#tmp.txt contenant les informations de l'ensemble des services disponibles sur le réseau
+#On a comme information (le nom de l'hôte du service, le nom du service et le port à utiliser pour y accéder
+
 import os
 import json
 
+#On lance la commande sytème avahi-browser pour obtenir l'ensemble des services disponibles sur le réseau
 var = os.popen("avahi-browse -atpr|grep -E '=;.*_inventory._tcp'").read().split('\n')
 
+#On récupére et on stocke les informations de chaque service
 eqpmt = {}
 for vv in var:
 	vv = vv.split(';')
@@ -12,6 +18,8 @@ for vv in var:
 		key = "{0}_{1}_{2}".format(vv[-4], vv[3], vv[-2])
 		eqpmt[key] = [vv[-4], vv[3], vv[-2]] #hostname, service, port
 
+#On enregistre les informations de chaque service en éliminant ceux de l'hôte qui lance le script
+#le but étant de n'avoir que les informations sur les services des autres équipements
 host = os.popen("hostname").read().split('\n')[0]
 fichier = open("tmp.txt", 'w')
 for key in eqpmt.keys():
@@ -19,8 +27,3 @@ for key in eqpmt.keys():
 		fichier.write("{0};{1};{2}\n".format(eqpmt[key][0], eqpmt[key][1], eqpmt[key][2]))
 
 fichier.close()
-
-#for key in eqpmt.keys():
-	#if eqpmt[key][0].split('.')[0] == host:
-		#os.popen("./server {} &".format(eqpmt[key][2]))
-		#msg = os.popen("./server 16 &").read()
